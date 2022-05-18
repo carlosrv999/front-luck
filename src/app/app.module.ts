c8584ff6527d88fcb35e4aafab800f1563fc1c16
Carlos Ramirez <carlosrv125@icloud.com>
2022-05-18 17:45:56 -0500

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToolbarComponent } from 'src/toolbar/toolbar.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
